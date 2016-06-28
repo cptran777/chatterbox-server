4ab8d282e2d7e59dcb5ef8d031ab020b7f88dc6b
cptran777 <cptran14@gmail.com>
2016-06-27 21:20:40 -0700

    Completed all basic requirements
    First commit, code might need refractoring

--- server/request-handler.js
+++ server/request-handler.js
@@ -11,9 +11,55 @@ this file and include it in basic-server.js so that it actually works.
 *Hint* Check out the node module documentation at http://nodejs.org/api/modules.html.
 
 **************************************************************/
+var serverStorage = {
+  '/classes/messages': [],
+  '/send': []
+};
 
-var requestHandler = function(request, response) {
-  // Request and Response come from node's http module.
+module.exports = function(request, response) {
+  var method = request.method;
+  var url = request.url;
+  var body = [];
+  var headers = defaultCorsHeaders;
+  var statusCode;
+  headers['Content-Type'] = 'application/json';
+  if (request.method === 'POST') {
+    var res = [];
+
+    request.on('data', function(data) {
+      res.push(data); 
+      console.log(res);
+    });
+
+    request.on('end', function() {
+      statusCode = 201;
+
+      if (url === '/classes/messages') {
+        serverStorage['/classes/messages'].push(JSON.parse(res));
+      } else if (url === '/send') {
+        serverStorage['/send'].push(JSON.parse(res));
+      } else {
+        statusCode = 404;
+      }
+
+      response.writeHead(statusCode, headers);
+
+      var responseBody = {
+        method: method,
+        url: url,
+        results: res
+      };
+
+
+      if (response.write) {
+        response.write(JSON.stringify(responseBody));
+        response.end();
+      } else {
+        response.end(JSON.stringify(responseBody));                          
+      }
+    });
+  } else {
+  // Request and Response access-control-allow-methods from node's http module.
   //
   // They include information about both the incoming request, such as
   // headers and URL, and about the outgoing response, such as its status
@@ -27,23 +73,84 @@ var requestHandler = function(request, response) {
   // Adding more logging to your server can be an easy way to get passive
   // debugging help, but you should always be careful about leaving stray
   // console.logs in your code.
-  console.log('Serving request type ' + request.method + ' for url ' + request.url);
+  // console.log('Serving request type ' + request.method + ' for url ' + request.url);
 
   // The outgoing status.
-  var statusCode = 200;
+    statusCode = 200;
 
   // See the note below about CORS headers.
-  var headers = defaultCorsHeaders;
+  
 
   // Tell the client we are sending them plain text.
   //
   // You will need to change this if you are sending something
   // other than plain text, like JSON or HTML.
-  headers['Content-Type'] = 'text/plain';
+ 
 
   // .writeHead() writes to the request line and headers of the response,
   // which includes the status and all headers.
-  response.writeHead(statusCode, headers);
+    request.on('error', function(err) {
+      statusCode = 404;
+      console.log(err);
+    });
+
+    request.on('data', function(chunk) {
+      body.push(chunk);
+    });
+
+    request.on('end', function() {
+      var results;
+
+      if (url === '/classes/messages') {
+        results = serverStorage['/classes/messages'];
+      } else if (url === '/send') {
+        results = serverStorage['/send'];
+      } else {
+        statusCode = 404;
+      }
+
+      if (response.on) {
+        response.on('error', function(err) {
+        });
+      }
+
+      response.writeHead(statusCode, headers);
+
+      if (body.username) {
+        results.filter(function(obj) {
+          return obj.username === body.username;
+        });
+      } 
+
+      var responseBody = {
+        method: method,                                                                                                                                                                                                                                                                                                                                          
+        url: url,
+        results: results
+      };
+
+      if (response.write) {
+        response.write(JSON.stringify(responseBody));
+        response.end();
+      } else {
+        response.end(JSON.stringify(responseBody));                          
+      }
+    });
+                                                                                                                                                                                                                           
+
+  /*
+  if (first parse piece === 'classes') {
+    if (second parse piece === 'messages') {
+      serverStorage['messages'].push(data);
+    }
+  } else {
+  if (first parse piece === 'send') {
+    serverStorage['send'].push(data);
+  }
+  }
+  */
+
+
+
 
   // Make sure to always call response.end() - Node may not send
   // anything back to the client until you do. The string you pass to
@@ -52,8 +159,9 @@ var requestHandler = function(request, response) {
   //
   // Calling .end "flushes" the response's internal buffer, forcing
   // node to actually send all the data over to the client.
-  response.end('Hello, World!');
+  }
 };
+
 
 // These headers will allow Cross-Origin Resource Sharing (CORS).
 // This code allows this server to talk to websites that
@@ -70,4 +178,5 @@ var defaultCorsHeaders = {
   'access-control-allow-headers': 'content-type, accept',
   'access-control-max-age': 10 // Seconds.
 };
+
 
